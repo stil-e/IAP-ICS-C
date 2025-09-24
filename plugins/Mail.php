e88dc992449948c012f422be158b41609eb2a379
stile <brian.ikubu@strathmore.edu>
2025-09-24 11:39:38 +0300

    Additions

--- plugins/Mail.php
+++ plugins/Mail.php
@@ -17,7 +17,7 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'brian.ikubu@strathmore.edu';                     //SMTP username
+    $mail->Username   = 'brian.ikubU@strathmore.edu';                     //SMTP username
     $mail->Password   = 'boie nbzt fdye plbr';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`

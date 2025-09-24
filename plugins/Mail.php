c4548a9c5a2b0e503adcfe20388d516c47fcd4a0
stile <brian.ikubu@strathmore.edu>
2025-09-24 11:46:24 +0300

    Removal of some things

--- plugins/Mail.php
+++ plugins/Mail.php
@@ -24,17 +24,7 @@ try {
 
     //Recipients
     $mail->setFrom('Bmathara01@gmail.com', 'Brian');
-    //$mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-    //$mail->addAddress('ellen@example.com');               //Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
 
-    //Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-    //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Welcome to ICS C Community';
     $mail->Body    = 'This is a new semester';
